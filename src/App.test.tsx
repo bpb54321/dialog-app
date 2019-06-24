fd2bcffb7f7e59ca31f727cdca0e2adfa3dfc1ed
Brian Blosser <bpb54321@gmail.com>
2019-06-23 22:08:58 -0400

    Deleted some unused tests.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -45,35 +45,6 @@ describe('App', () => {
 
   });
 
-  // it("displays the lines up to but not including the first line of Role 1", function () {
-  //   expect(app.instance().state.currentDialog).toEqual({
-  //     name: "testDialog",
-  //     roles: ["Role 0", "Role 1"],
-  //     lines: [
-  //       {
-  //         text: "Text for line 0.",
-  //         role: "Role 0",
-  //         key: 0,
-  //       },
-  //       {
-  //         text: "Text for line 1.",
-  //         role: "Role 1",
-  //         key: 1,
-  //       },
-  //       {
-  //         text: "Text for line 2.",
-  //         role: "Role 0",
-  //         key: 2,
-  //       },
-  //       {
-  //         text: "Text for line 3.",
-  //         role: "Role 1",
-  //         key: 3,
-  //       },
-  //     ],
-  //   });
-  // });
-
   it('should display a role picker form when rendered', function () {
     expect(wrapper.queryByTestId("role-picker")).not.toBeNull();
   });
@@ -111,51 +82,6 @@ describe('App', () => {
     line0 = await waitForElement(() => app.getByText(testDialog.lines[0].text));
   });
 
-
-  // it("has a default current line number of 0", () => {
-  //   expect(app.instance().state.userRoleLineIndex).toEqual(0);
-  // });
-  //
-  // it("should print all the lines up to the first line of the User1", function () {
-  //   // Given that the user role is User1
-  //   // And that User1 has the first line in the dialogue
-  //   app.instance().setState({
-  //     userRole: "Role 0",
-  //   });
-  //
-  //   // Then I expect that the first line in the dialogue will be displayed
-  //   expect(app.contains(<Line text={"Text for line 0."} key={0} />)).toBe(true);
-  // });
-  //
-  // it("should print all the lines up to the first line of the User1", function () {
-  //   // Given that the user role is User2
-  //   // And that User2 has the second line in the dialogue
-  //   app.instance().setState({
-  //     userRole: "Role 1",
-  //   });
-  //
-  //   // Then I expect that the first and second line in the dialogue will be displayed
-  //   expect(app.contains(<Line text={"Text for line 0."} key={0} />)).toBe(true);
-  //   expect(app.contains(<Line text={"Text for line 1."} key={1} />)).toBe(true);
-  // });
-  //
-  // it('should print all the lines up to the second line of Role 0 if userRoleLineIndex = 1 and userRole = "Role 0" ', function () {
-  //   app.instance().setState({
-  //     userRole: "Role 1",
-  //     userRoleLineIndex: 1,
-  //   });
-  //
-  //   expect(app.contains(<Line text={"Text for line 0."} key={0} />)).toBe(true);
-  //   expect(app.contains(<Line text={"Text for line 1."} key={1} />)).toBe(true);
-  //   expect(app.contains(<Line text={"Text for line 2."} key={2} />)).toBe(true);
-  //
-  // });
-  //
-  // it("should have a button with text 'Show Next Line'", function () {
-  //   const button = app.find(Button);
-  //   expect(button).toHaveLength(1);
-  //   expect(button.prop("text")).toBe("Show Next Line");
-  // });
 
 });
 
