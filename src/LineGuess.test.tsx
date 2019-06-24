50d35cebf42e69abe1e20a34e0846a8161c0986e
Brian Blosser <bpb54321@gmail.com>
2019-06-24 17:48:44 -0400

    Did TDD on the elements in LineGuess.

--- src/LineGuess.test.tsx
+++ src/LineGuess.test.tsx
@@ -7,12 +7,25 @@ import {
 } from "@testing-library/react";
 import LineGuess from "./LineGuess";
 import {testDialog} from "./data/test-dialog";
+import {Simulate} from "react-dom/test-utils";
 
 describe('LineGuess', () => {
     it('should render a form which contains a label, text input, and submit input', function () {
-        const lineGuess = render(<LineGuess lineToGuess={testDialog.lines[0]} />);
-        lineGuess.getByTestId("line-guess__label");
-        lineGuess.getByTestId("line-guess__text-input");
-        lineGuess.getByTestId("line-guess__submit");
+        const wrapper = render(<LineGuess lineToGuess={testDialog.lines[0]} />);
+
+        const lineGuess = wrapper.getByTestId("line-guess");
+        expect(lineGuess.tagName).toBe("FORM");
+
+        const label = wrapper.getByTestId("line-guess__label");
+        expect(label.tagName).toBe("LABEL");
+
+        const textInput = wrapper.getByTestId("line-guess__text-input");
+        expect(textInput.tagName).toBe("INPUT");
+        expect((textInput as HTMLInputElement).type).toBe("text");
+
+        const submit = wrapper.getByTestId("line-guess__submit");
+        expect(submit.tagName).toBe("INPUT");
+        expect((submit as HTMLInputElement).type).toBe("submit");
+        expect((submit as HTMLInputElement).value).toBe("Guess");
     });
 });
--- src/LineGuess.tsx
+++ src/LineGuess.tsx
@@ -14,9 +14,12 @@ export default class LineGuess extends React.Component<Props, State> {
 
     render() {
         return (
-            <div data-testid={"line-guess"}>
-                {`${this.props.lineToGuess.role}, Line ${this.props.lineToGuess.key} Guess`}
-            </div>
+            <form data-testid={"line-guess"}>
+                <label htmlFor="line-guess__text-input" data-testid={"line-guess__label"}></label>
+                <input type="text" data-testid={"line-guess__text-input"} id={"line-guess__text-input"} />
+                {/*{`${this.props.lineToGuess.role}, Line ${this.props.lineToGuess.key} Guess`}*/}
+                <input type="submit" data-testid={"line-guess__submit"} value={"Guess"}/>
+            </form>
         );
     }
 }
