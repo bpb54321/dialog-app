a285625f8ad361aaee53f71bf999b2c65a581bad
Brian Blosser <bpb54321@gmail.com>
2019-07-09 11:30:54 -0400

    Did some cleanup.

--- src/MockSpeechRecognition.ts
+++ src/MockSpeechRecognition.ts
@@ -1,23 +1,4 @@
 /**
- * Returns a mocked version of a SpeechRecognitionEvent.
- * @link (https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognitionEvent)
- * @param transcript - The string that you want the primary SpeechRecognitionAlternative in your mocked
- * SpeechRecognitionEvent to have.
- */
-export function createMockSpeechRecognitionEvent(transcript: string): any {
-  return {
-    results: [
-      [
-        {
-          transcript: transcript,
-        },
-      ],
-    ],
-  };
-}
-
-
-/**
  * Mocks the SpeechRecognition interface of the Web Speech API - currently only available on Chrome
  * @link (https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognition)
  */
