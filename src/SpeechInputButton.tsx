2cf242434b752259705a75194e4ee005353a3781
Brian Blosser <bpb54321@gmail.com>
2019-07-10 15:14:43 -0400

    Passed the button text toggle test.

--- src/SpeechInputButton.tsx
+++ src/SpeechInputButton.tsx
@@ -1,23 +1,38 @@
 import React from 'react';
 
+enum ButtonText {
+  StartSpeechInput = "Start Speech Input",
+  StopSpeechInput = "Stop Speech Input",
+}
+
 interface Props {
   speechRecognition: any;
 }
 
 interface State {
+  text: string;
 }
 
 export default class SpeechInputButton extends React.Component<Props, State> {
 
-  state = {};
+  state = {
+    text: ButtonText.StartSpeechInput,
+  };
 
   handleToggleSpeechInput = () => {
     this.props.speechRecognition.start();
+    if (this.state.text === ButtonText.StartSpeechInput) {
+      this.setState({text: ButtonText.StopSpeechInput});
+    } else {
+      this.setState({text: ButtonText.StartSpeechInput});
+    }
   };
 
   render() {
     return (
-      <button onClick={this.handleToggleSpeechInput}>Start Speech Input</button>
+      <button onClick={this.handleToggleSpeechInput}>
+        {this.state.text}
+      </button>
     );
   }
 }
