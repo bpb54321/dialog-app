720cfb61eacd2b2463f5342696c30fd9ad5a7889
Brian Blosser <bpb54321@gmail.com>
2019-07-12 10:31:15 -0400

    Added testDialogs.

--- src/data/test-dialog.ts
+++ src/data/test-dialog.ts
@@ -30,3 +30,66 @@ export const testDialog: Dialog = {
         },
     ],
 };
+
+export const testDialogs: Dialog[] = [
+  {
+    name: "Dialog 0",
+    roles: ["Role 0.0", "Role 0.1"],
+    lines: [
+      {
+        text: "Text for line 0.0.",
+        guess: "",
+        role: "Role 0.0",
+        key: 0,
+      },
+      {
+        text: "Text for line 0.1.",
+        guess: "",
+        role: "Role 0.1",
+        key: 1,
+      },
+      {
+        text: "Text for line 0.2.",
+        guess: "",
+        role: "Role 0.0",
+        key: 2,
+      },
+      {
+        text: "Text for line 0.3.",
+        guess: "",
+        role: "Role 0.1",
+        key: 3,
+      },
+    ],
+  },
+  {
+    name: "Dialog 1",
+    roles: ["Role 1.0", "Role 1.1"],
+    lines: [
+      {
+        text: "Text for line 1.0.",
+        guess: "",
+        role: "Role 1.0",
+        key: 0,
+      },
+      {
+        text: "Text for line 1.1.",
+        guess: "",
+        role: "Role 1.1",
+        key: 1,
+      },
+      {
+        text: "Text for line 1.2.",
+        guess: "",
+        role: "Role 1.0",
+        key: 2,
+      },
+      {
+        text: "Text for line 1.3.",
+        guess: "",
+        role: "Role 1.1",
+        key: 3,
+      },
+    ],
+  },
+];
