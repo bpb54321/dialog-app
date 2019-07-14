9c23fcf657104a22ee7e3c5d91e296937a58aa1b
Brian Blosser <bpb54321@gmail.com>
2019-07-14 14:42:00 -0400

    Added line numbers to the test data.

--- src/data/test-dialog.ts
+++ src/data/test-dialog.ts
@@ -37,28 +37,32 @@ export const testDialogs: Dialog[] = [
     roles: ["Role 0.0", "Role 0.1"],
     lines: [
       {
+        id: 0,
         text: "Text for line 0.0.",
         guess: "",
         role: "Role 0.0",
-        key: 0,
+        number: 0
       },
       {
+        id: 1,
         text: "Text for line 0.1.",
         guess: "",
         role: "Role 0.1",
-        key: 1,
+        number: 1
       },
       {
+        id: 2,
         text: "Text for line 0.2.",
         guess: "",
         role: "Role 0.0",
-        key: 2,
+        number: 2
       },
       {
+        id: 3,
         text: "Text for line 0.3.",
         guess: "",
         role: "Role 0.1",
-        key: 3,
+        number: 3
       },
     ],
   },
@@ -67,28 +71,32 @@ export const testDialogs: Dialog[] = [
     roles: ["Role 1.0", "Role 1.1"],
     lines: [
       {
+        id: 0,
         text: "Text for line 1.0.",
         guess: "",
         role: "Role 1.0",
-        key: 0,
+        number: 0
       },
       {
+        id: 1,
         text: "Text for line 1.1.",
         guess: "",
         role: "Role 1.1",
-        key: 1,
+        number: 1
       },
       {
+        id: 2,
         text: "Text for line 1.2.",
         guess: "",
         role: "Role 1.0",
-        key: 2,
+        number: 2
       },
       {
+        id: 3,
         text: "Text for line 1.3.",
         guess: "",
         role: "Role 1.1",
-        key: 3,
+        number: 3
       },
     ],
   },
