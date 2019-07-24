9b8445736209e8c613b65ed195c27f51ad067d3d
Brian Blosser <bpb54321@gmail.com>
2019-07-24 11:21:29 -0400

    Refactoring the fetchData function.

--- src/utils/fetch-data.ts
+++ src/utils/fetch-data.ts
@@ -1,31 +1,54 @@
-import GraphqlError from "../types/GraphqlError";
+import {GlobalContextObject} from "../contexts/GlobalContext";
 
-export default function fetchData(
-  query: string, token: string = "", apiEndpoint: string, resultCallback: (body: any) => void,
-  errorMessageCallback: (errorMessage: string) => void
-): void {
-  fetch(apiEndpoint, {
+// export default function fetchData(
+//   query: string, token: string = "", apiEndpoint: string, resultCallback: (body: any) => void,
+//   errorMessageCallback: (errorMessage: string) => void
+// ): void {
+//   fetch(apiEndpoint, {
+//     method: "POST",
+//     body: JSON.stringify({
+//       query: query,
+//     }),
+//     headers: {
+//       "Authorization": token ? `Bearer ${token}` : "",
+//       "Content-Type": "application/json",
+//     },
+//     mode: "cors",
+//   }).then((response) => {
+//     return response.json();
+//   }).then((body) => {
+//     if (body.errors) {
+//       let errorMessage = body.errors.reduce((accumulator: string, error: GraphqlError) => {
+//         return accumulator + " " + error.message;
+//       }, "");
+//       errorMessageCallback(errorMessage);
+//     } else {
+//       resultCallback(body);
+//     }
+//   }).catch((error) => {
+//     console.log(error);
+//   });
+// }
+
+export default async function fetchData(query: string, topLevelQueryField: string,
+                                        globalContext: GlobalContextObject) {
+
+  const {data} = globalContext;
+
+  const response = await fetch(data.apiEndpoint, {
     method: "POST",
     body: JSON.stringify({
       query: query,
     }),
     headers: {
-      "Authorization": token ? `Bearer ${token}` : "",
+      "Authorization": data.token ? `Bearer ${data.token}` : "",
       "Content-Type": "application/json",
     },
     mode: "cors",
-  }).then((response) => {
-    return response.json();
-  }).then((body) => {
-    if (body.errors) {
-      let errorMessage = body.errors.reduce((accumulator: string, error: GraphqlError) => {
-        return accumulator + " " + error.message;
-      }, "");
-      errorMessageCallback(errorMessage);
-    } else {
-      resultCallback(body);
-    }
-  }).catch((error) => {
-    console.log(error);
   });
+
+  const body = await response.json();
+
+  return body.data[topLevelQueryField];
 }
+
