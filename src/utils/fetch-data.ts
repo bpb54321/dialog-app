3c290be61f11214b2cd502cba3264b7abb026af2
Brian Blosser <bpb54321@gmail.com>
2019-07-24 11:49:29 -0400

    Deleted the old commented-out fetchData.

--- src/utils/fetch-data.ts
+++ src/utils/fetch-data.ts
@@ -1,35 +1,5 @@
 import {GlobalContextObject} from "../contexts/GlobalContext";
 
-// export default function fetchData(
-//   query: string, token: string = "", apiEndpoint: string, resultCallback: (body: any) => void,
-//   errorMessageCallback: (errorMessa ge: string) => void
-// ): void {
-//   fetch(apiEndpoint, {
-//     method: "POST",
-//     body: JSON.stringify({
-//       query: query,
-//     }),
-//     headers: {
-//       "Authorization": token ? `Bearer ${token}` : "",
-//       "Content-Type": "application/json",
-//     },
-//     mode: "cors",
-//   }).then((response) => {
-//     return response.json();
-//   }).then((body) => {
-//     if (body.errors) {
-//       let errorMessage = body.errors.reduce((accumulator: string, error: GraphqlError) => {
-//         return accumulator + " " + error.message;
-//       }, "");
-//       errorMessageCallback(errorMessage);
-//     } else {
-//       resultCallback(body);
-//     }
-//   }).catch((error) => {
-//     console.log(error);
-//   });
-// }
-
 export default async function fetchData(query: string, topLevelQueryField: string,
                                         globalContext: GlobalContextObject) {
 
