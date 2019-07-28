4838001f4b376e27af1b49f08c914885a0323d0d
Brian Blosser <bpb54321@gmail.com>
2019-07-28 19:34:10 -0400

    Added better HTML structure to the app, and a top bar nav.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 /* global SpeechRecognition */
 import React from 'react';
-import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Redirect, Switch, Link} from 'react-router-dom';
 import './App.css';
 
 import {Dialog} from "./types/Dialog";
@@ -42,66 +42,76 @@ export class App extends React.Component<AppProps, AppState> {
 
   render() {
     return (
-      <GlobalProvider speechRecognition={this.props.speechRecognition}>
-        <BrowserRouter>
-            <GlobalConsumer>
-              {(context: GlobalContextObject) => {
-                if (!context.data.token) {
+      <body className={"app"}>
+        <GlobalProvider speechRecognition={this.props.speechRecognition}>
+          <BrowserRouter>
+              <GlobalConsumer>
+                {(context: GlobalContextObject) => {
                   return (
-                    <Switch>
-                      <Route path={"/auth"} component={AuthPage}/>
-                      <Route path={"/sign-up"} component={SignupPage}/>
-                      <Redirect from={"/"} to={"/auth"} />
-                    </Switch>
+                    <>
+                      <header>
+                        <nav className="navbar navbar-light bg-light">
+                          <Link className="navbar-brand app__home-link" to={"/"}>Dialog Practice</Link>
+                        </nav>
+                      </header>
+                      <main className={"app__main"}>
+                        {
+                          (!context.data.token) ?
+                            <Switch>
+                              <Route path={"/auth"} component={AuthPage}/>
+                              <Route path={"/sign-up"} component={SignupPage}/>
+                              <Redirect from={"/"} to={"/auth"} />
+                            </Switch>
+                          :
+                            <Switch>
+                              <Redirect exact from={"/"} to={"/dialogs"}/>
+                              <Redirect from={"/auth"} to={"/dialogs"}/>
+                              {/* Sign-up page after account creation, gives user feedback that account was created */}
+                              <Route path={"/sign-up"} component={SignupPage}/>
+                              <Route
+                                exact
+                                path={"/dialogs"}
+                                render={(routeProps) => {
+                                  return (<DialogListPage {...routeProps} context={context}/>);
+                                }}
+                              />
+                              <Route
+                                path={"/dialogs/:dialogId/choose-role"}
+                                render={(routeProps) => {
+                                  return (<ChooseRolePage {...routeProps}/>);
+                                }}
+                              />
+                              <Route
+                                path={"/dialogs/:dialogId/practice"}
+                                render={(routeProps) => {
+                                  return (<PracticePage
+                                    {...routeProps}
+                                    context={context}
+                                  />);
+                                }}
+                              />
+                              <Route
+                                path={"/dialogs/:dialogId/edit"}
+                                render={(routeProps) => {
+                                  return (
+                                    <DialogEditPage
+                                      {...routeProps}
+                                      context={context}
+                                    />
+                                  );
+                                }}
+                              />
+                            </Switch>
+                        }
+                      </main>
+                    </>
                   );
-                } else {
-                  return (
-                    <Switch>
-                      <Redirect exact from={"/"} to={"/dialogs"}/>
-                      <Redirect from={"/auth"} to={"/dialogs"}/>
-                      {/* Sign-up page after account creation, gives user feedback that account was created */}
-                      <Route path={"/sign-up"} component={SignupPage}/>
-                      <Route
-                        exact
-                        path={"/dialogs"}
-                        render={(routeProps) => {
-                          return (<DialogListPage {...routeProps} context={context}/>);
-                        }}
-                      />
-                      <Route
-                        path={"/dialogs/:dialogId/choose-role"}
-                        render={(routeProps) => {
-                          return (<ChooseRolePage {...routeProps}/>);
-                        }}
-                      />
-                      <Route
-                        path={"/dialogs/:dialogId/practice"}
-                        render={(routeProps) => {
-                          return (<PracticePage
-                            {...routeProps}
-                            context={context}
-                          />);
-                        }}
-                      />
-                      <Route
-                        path={"/dialogs/:dialogId/edit"}
-                        render={(routeProps) => {
-                          return (
-                            <DialogEditPage
-                              {...routeProps}
-                              context={context}
-                            />
-                          );
-                        }}
-                      />
-                    </Switch>
-                  );
-                }
-              }}
-            </GlobalConsumer>
+                }}
+              </GlobalConsumer>
 
-        </BrowserRouter>
-      </GlobalProvider>
+          </BrowserRouter>
+        </GlobalProvider>
+      </body>
     );
   }
 }
