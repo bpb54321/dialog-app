993050c0f099f340c1742184aed98e56ed920e23
Brian Blosser <bpb54321@gmail.com>
2019-07-31 16:37:29 -0400

    Converted SignupPage to use hooks.

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,43 +1,32 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import {GlobalContextObject} from "../contexts/GlobalStateContext";
-import {GlobalConsumer} from "../contexts/GlobalStateContext";
+import {useGlobalState} from "../contexts/GlobalStateContext";
 import {SignupForm} from "../components/SignupForm";
 
 interface Props {
 
 }
 
-interface State {
-}
+export const SignupPage: React.FunctionComponent<Props> = (props) => {
 
-export default class SignupPage extends React.Component<Props, State> {
+  const globalState = useGlobalState();
 
-  render() {
-    return (
-      <GlobalConsumer>
-        {(context: GlobalContextObject) => {
-          return (
-            <>
-              <h1>Sign Up</h1>
-              {
-                context.data.token
-                ?
-                  <p>
-                    User account successfully created! Visit the <Link to={"/dialogs"}>Dialog Page</Link> to create
-                    some dialogs.
-                  </p>
-                :
-                  <>
-                    <SignupForm/>
-                    <Link to={"/auth"}>Login</Link>
-                  </>
-              }
-            </>
-          );
-
-        }}
-      </GlobalConsumer>
-    );
-  }
-}
+  return (
+    <>
+      <h1>Sign Up</h1>
+      {
+        globalState.token
+        ?
+          <p>
+            User account successfully created! Visit the <Link to={"/dialogs"}>Dialog Page</Link> to create
+            some dialogs.
+          </p>
+        :
+          <>
+            <SignupForm/>
+            <Link to={"/auth"}>Login</Link>
+          </>
+      }
+    </>
+  );
+};
