67ba344bfeb3e5883dd399eb5dd4fdd3dd0f1831
Brian Blosser <bpb54321@gmail.com>
2019-07-31 14:17:19 -0400

    Refactored PracticePage as a Functional Component.

--- src/pages/PracticePage.tsx
+++ src/pages/PracticePage.tsx
@@ -1,28 +1,12 @@
-import React from 'react';
-import {GlobalConsumer, GlobalContextObject} from "../contexts/GlobalStateContext";
+import React, {useEffect, useState} from 'react';
+import {useGlobalState} from "../contexts/GlobalStateContext";
 import fetchData from "../utils/fetch-data";
 import Role from "../types/Role";
 import LineData from "../types/LineData";
 import {Dialog} from "../types/Dialog";
 import {InteractionMode} from "../types/InteractionMode";
 import ListOfLines from "../ListOfLines";
-import LineGuess from "../LineGuess";
-
-interface Props {
-  context: GlobalContextObject;
-  match: any;
-  location: any;
-  history: any;
-}
-
-interface State {
-  userLineNumberIndex: number;
-  userLineNumbers: number[];
-  currentLineNumber: number;
-  dialog: Dialog;
-  mode: InteractionMode;
-  errorMessage: string;
-}
+import {LineGuess} from "../LineGuess";
 
 //region singleDialogQuery
 const singleDialogQuery = `
@@ -43,39 +27,93 @@ const singleDialogQuery = `
     `;
 //endregion
 
-export default class PracticePage extends React.Component<Props, State> {
+interface Props {
+  match: any;
+  location: any;
+  history: any;
+}
 
-  state = {
-    userLineNumberIndex: 0,
-    userLineNumbers: [],
-    currentLineNumber: 0,
-    dialog: {
-      id: "",
-      name: "",
-      lines: [],
-      roles: [],
-      languageCode: "",
-    },
-    errorMessage: "",
-    mode: InteractionMode.PracticingLines,
+export const PracticePage: React.FunctionComponent<Props> = (props) => {
+  const [userLineNumberIndex, setUserLineNumberIndex] = useState(0);
+  const [userLineNumbers, setUserLineNumbers] = useState([] as number[]);
+  const [currentLineNumber, setCurrentLineNumber] = useState(0);
+  const [dialog, setDialog] = useState({} as Dialog);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [mode, setMode] = useState(InteractionMode.PracticingLines);
+
+  const globalState = useGlobalState();
+
+  /**
+   * Given a dialog and a role, returns an array of the line numbers that the role has in the dialog.
+   *
+   * @param dialog {Dialog} A dialog.
+   * @param role {Role} The role in the dialog that the user has picked to be.
+   *
+   * @return {number[]} An array of line numbers of the lines that are assigned to the given role in the dialog.
+   */
+  const calculateUserLineNumbers = (dialog: Dialog, role: Role): number[] => {
+    const {lines} = dialog;
+
+    let userLines: LineData[] = lines.filter((line: LineData) => {
+      return (line.role.id === role.id);
+    });
+
+    return userLines.map((line: LineData) => {
+      return line.number;
+    });
   };
 
-  async componentDidMount(): Promise<void> {
+  const addGuessToCurrentLineAndIncrementLineNumber = (lineGuess: string) => {
 
+    const linesUpdatedWithGuess = dialog.lines.map((line: LineData) => {
+      if (line.number === currentLineNumber) {
+        line.guess = lineGuess;
+      }
+
+      return line;
+    });
+
+    let nextLineIndex = userLineNumberIndex + 1;
+
+    let nextMode: InteractionMode;
+    let nextLineNumber: number;
+
+    if (nextLineIndex < userLineNumbers.length) {
+
+      nextMode = InteractionMode.PracticingLines;
+      nextLineNumber = userLineNumbers[nextLineIndex];
+
+    } else {
+
+      nextMode = InteractionMode.DialogComplete;
+      nextLineNumber = dialog.lines.slice(-1)[0].number; // Last line in dialog
+
+    }
+
+    setDialog({
+      ...dialog,
+      lines: linesUpdatedWithGuess,
+    });
+    setUserLineNumberIndex(nextLineIndex);
+    setCurrentLineNumber(nextLineNumber);
+    setMode(nextMode);
+  };
+
+  useEffect(() => {
     const {
       params: {
         dialogId
       }
-    } = this.props.match;
+    } = props.match;
 
     const queryVariables = {
       id: dialogId,
     };
 
-    fetchData(singleDialogQuery, queryVariables, "dialog", this.props.context).then((dialog) => {
+    fetchData(singleDialogQuery, queryVariables, "dialog", globalState).then((dialog) => {
 
       // Calculate the user line numbers
-      const userLineNumbers = this.calculateUserLineNumbers(dialog, this.props.context.data.chosenRole);
+      const userLineNumbers = calculateUserLineNumbers(dialog, globalState.chosenRole);
 
       let currentLineNumber: number;
       let mode: InteractionMode;
@@ -88,120 +126,44 @@ export default class PracticePage extends React.Component<Props, State> {
         currentLineNumber = dialog.lines.slice(-1)[0].number;
       }
 
-      this.setState({
-        dialog: dialog,
-        userLineNumbers: userLineNumbers,
-        mode,
-        currentLineNumber,
-      });
+      setDialog(dialog);
+      setUserLineNumbers(userLineNumbers);
+      setMode(mode);
+      setCurrentLineNumber(currentLineNumber);
 
-      const speechRecognition = this.props.context.data.speechRecognition;
+      const speechRecognition = globalState.speechRecognition;
       speechRecognition.lang = dialog.languageCode;
 
     }).catch((error) => {
-      this.setState({
-        errorMessage: error.message
-      });
+      setErrorMessage(error.message);
     })
+  });
 
+  switch (mode) {
+    case InteractionMode.PracticingLines:
+      return (
+        <>
+          <ListOfLines
+            dialog={dialog}
+            lastLineToDisplay={currentLineNumber - 1}
+          />
+          <LineGuess
+            addLineGuessToLastLine={addGuessToCurrentLineAndIncrementLineNumber}
+          />
+          {errorMessage ? <p>{errorMessage}</p> : null}
+        </>
+      );
+    case InteractionMode.DialogComplete:
+      return (
+        <>
+          <ListOfLines
+            dialog={dialog}
+            lastLineToDisplay={currentLineNumber}
+          />
+          {errorMessage ? <p>{errorMessage}</p> : null}
+        </>
+      );
+    default:
+      return null;
   }
-
-  /**
-   * Given a dialog and a role, returns an array of the line numbers that the role has in the dialog.
-   *
-   * @param dialog {Dialog} A dialog.
-   * @param role {Role} The role in the dialog that the user has picked to be.
-   *
-   * @return {number[]} An array of line numbers of the lines that are assigned to the given role in the dialog.
-   */
-  calculateUserLineNumbers(dialog: Dialog, role: Role): number[] {
-    const {lines} = dialog;
-
-    let userLines: LineData[] = lines.filter((line: LineData) => {
-      return (line.role.id === role.id);
-    });
-
-    return userLines.map((line: LineData) => {
-      return line.number;
-    });
-  }
-
-  addGuessToCurrentLineAndIncrementLineNumber = (lineGuess: string) => {
-    this.setState((previousState: State) => {
-
-      const {dialog} = previousState;
-
-      const linesUpdatedWithGuess = dialog.lines.map((line: LineData) => {
-          if (line.number === this.state.currentLineNumber) {
-            line.guess = lineGuess;
-          }
-
-          return line;
-      });
-
-      let nextLineIndex: number = previousState.userLineNumberIndex + 1;
-
-      let nextMode: InteractionMode;
-      let nextLineNumber: number;
-
-      if (nextLineIndex < previousState.userLineNumbers.length) {
-
-        nextMode = InteractionMode.PracticingLines;
-        nextLineNumber = previousState.userLineNumbers[nextLineIndex];
-
-      } else {
-
-        nextMode = InteractionMode.DialogComplete;
-        nextLineNumber = dialog.lines.slice(-1)[0].number; // Last line in dialog
-
-      }
-
-      return ({
-        ...previousState,
-        dialog: {
-          ...dialog,
-          lines: linesUpdatedWithGuess,
-        },
-        userLineNumberIndex: nextLineIndex,
-        currentLineNumber: nextLineNumber,
-        mode: nextMode,
-      });
-    });
-  };
-
-  render() {
-    return (
-      <>
-        <GlobalConsumer>
-          {(context: GlobalContextObject) => {
-
-            switch (this.state.mode) {
-                case InteractionMode.PracticingLines:
-                  return (
-                    <>
-                      <ListOfLines
-                        dialog={this.state.dialog}
-                        lastLineToDisplay={this.state.currentLineNumber - 1}
-                      />
-                      <LineGuess
-                        userRole={context.data.chosenRole}
-                        addLineGuessToLastLine={this.addGuessToCurrentLineAndIncrementLineNumber}
-                        speechRecognition={context.data.speechRecognition}
-                      />
-                    </>
-                  );
-                case InteractionMode.DialogComplete:
-                  return (
-                    <ListOfLines
-                      dialog={this.state.dialog}
-                      lastLineToDisplay={this.state.currentLineNumber}
-                    />
-                  );
-              }
-          }}
-        </GlobalConsumer>
-        {this.state.errorMessage ? <p>{this.state.errorMessage}</p> : null}
-      </>
-    );
-  }
-}
+};
