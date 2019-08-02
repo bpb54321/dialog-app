0314d5de1cc3dd88f2e87e219dd7369c0db92207
Brian Blosser <bpb54321@gmail.com>
2019-08-02 10:39:52 -0400

    All tests now passing in PracticePage.test.

--- src/__tests__/PracticePage.test.tsx
+++ src/__tests__/PracticePage.test.tsx
@@ -52,6 +52,14 @@ jest.mock("../utils/fetch-data", () => {
               "id": "def",
               "name": "Role 2"
             }
+          },
+          {
+            "text": "This is the text for line 5.",
+            "number": 5,
+            "role": {
+              "id": "abc",
+              "name": "Role 1"
+            }
           }
         ]
       });
@@ -82,27 +90,6 @@ describe('PracticePage', () => {
 
   afterEach(cleanup);
 
-  test(`Given Role 2 is the chose role
-    Then it should print out Role 1's first line`, async function () {
-
-    chosenRole = {
-      "id": "def",
-      "name": "Role 2"
-    };
-
-    act(() => {
-      wrapper = render(
-        <GlobalProvider speechRecognition={mockSpeechRecognition}
-          children={<PracticePage match={match} chosenRole={chosenRole}/>}
-        />
-      );
-    });
-
-    await waitForElementToBeRemoved(() => wrapper.getByText(/waiting for data to load/i));
-
-    await waitForElement(() => wrapper.getByText(/this is the text for line 2/i));
-  });
-
   test(`Given Role 1 is the chosen role
     And the dialog is loaded
     Then no lines should be printed out
@@ -128,9 +115,41 @@ describe('PracticePage', () => {
     wrapper.getByPlaceholderText(/text of the next line for role 1/i);
   });
 
-  it(`When Role 1 enters a guess
+  test(`Given Role 2 is the chosen role
+    Then it should print out Role 1's first line
+    And the user should be presented with the line guess input`, async function () {
+
+    chosenRole = {
+      "id": "def",
+      "name": "Role 2"
+    };
+
+    act(() => {
+      wrapper = render(
+        <GlobalProvider speechRecognition={mockSpeechRecognition}
+          children={<PracticePage match={match} chosenRole={chosenRole}/>}
+        />
+      );
+    });
+
+    await waitForElementToBeRemoved(() => wrapper.getByText(/waiting for data to load/i));
+
+    await waitForElement(() => wrapper.getByText(/this is the text for line 1/i));
+
+    wrapper.getByPlaceholderText(/text of the next line for role 2/i);
+  });
+
+  it(`When Role 1 enters a guess for Line 1
     And he submits the guess
-    Then the submitted guess and the correct text for the line should be displayed.`, async function () {
+    Then the submitted guess and the correct text for the line should be displayed
+    And Role 1 should be presented with a guess input
+    When Role 1 enters a guess for Line 3
+    And he submits the guess
+    Then the submitted guess and the correct text for the line should be displayed
+    And Role 1 should be presented with a guess input
+    When Role 1 enters a guess for Line 5
+    Then all the line guesses and correct answers should be displayed
+    And there should be no guess input on the page`, async function () {
 
     chosenRole = {
       "id": "abc",
@@ -145,15 +164,13 @@ describe('PracticePage', () => {
       );
     });
 
-    const guessInput = await waitForElement(() => {
+    let guessInput = await waitForElement(() => {
       return wrapper.getByPlaceholderText(/text of the next line for role 1/i);
     });
 
-    const guess = "This is my guess for Line 1";
-
     fireEvent.change(guessInput, {
       target: {
-        value: guess,
+        value: "This is my guess for Line 1",
       }
     });
 
@@ -163,46 +180,41 @@ describe('PracticePage', () => {
       wrapper.getByText(/this is the text for line 1/i),
       wrapper.getByText(/this is my guess for line 1/i),
     ]);
-  });
 
-  it("When I guess my second line, the guess and the correct text for " +
-    "the first line and the second line are displayed.", function () {
+    // Second guess
+    fireEvent.change(wrapper.getByPlaceholderText(/text of the next line for role 1/i), {
+      target: {
+        value: "This is my guess for Line 3"
+      }
+    });
 
-    // // Click submit to confirm role selection of default role (Role 0)
-    // fireEvent.click(wrapper.getByText("Confirm Role Selection"));
-    //
-    // const guessInput = await waitForElement(() => wrapper.getByPlaceholderText(`Text of the next line for Role 0`));
-    // const submitButton = wrapper.getByDisplayValue("Submit Guess");
-    //
-    // const guessForLine0 = "This is my guess for Line 0";
-    //
-    // fireEvent.change(guessInput, {
-    //   target: {
-    //     value: guessForLine0,
-    //   }
-    // });
-    //
-    // fireEvent.click(submitButton);
-    //
-    // await waitForElement(() => [
-    //   wrapper.getByText(`Line text: ${testDialog.lines[0].text}`),
-    //   wrapper.getByText(`Guess: ${guessForLine0}`),
-    // ]);
-    //
-    // const guessForLine2 = "This is my guess for Line 2.";
-    //
-    // fireEvent.change(guessInput, {
-    //   target: {
-    //     value: guessForLine2,
-    //   }
-    // });
-    //
-    // fireEvent.click(submitButton);
-    //
-    // await waitForElement(() => [
-    //   wrapper.getByText(`Line text: ${testDialog.lines[2].text}`),
-    //   wrapper.getByText(`Guess: ${guessForLine2}`),
-    // ]);
+    fireEvent.click(wrapper.getByDisplayValue(/submit guess/i));
+
+    await waitForElement(() => [
+      wrapper.getByText(/this is the text for line 3/i),
+      wrapper.getByText(/this is my guess for line 3/i),
+    ]);
+
+    // Third guess
+    fireEvent.change(wrapper.getByPlaceholderText(/text of the next line for role 1/i), {
+      target: {
+        value: "This is my guess for Line 5"
+      }
+    });
+
+    fireEvent.click(wrapper.getByDisplayValue(/submit guess/i));
+
+    await waitForElement(() => [
+      wrapper.getByText(/this is the text for line 1/i),
+      wrapper.getByText(/this is the text for line 3/i),
+      wrapper.getByText(/this is the text for line 5/i),
+      wrapper.getByText(/this is my guess for line 1/i),
+      wrapper.getByText(/this is my guess for line 3/i),
+      wrapper.getByText(/this is my guess for line 5/i),
+    ]);
+
+    expect(wrapper.queryByPlaceholderText(/text of the next line for role 1/i)).toBeNull();
+
   });
 
   test("Then the app's SpeechRecognition object's language should be set to French.", function() {
