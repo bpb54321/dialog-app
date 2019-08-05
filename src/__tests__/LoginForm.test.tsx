a52304078846b4bb59d8bdf37d4051e11f46d2e1
Brian Blosser <bpb54321@gmail.com>
2019-08-04 20:44:47 -0400

    Wrote the initial test for the loading spinner in LoginForm.test.

--- src/__tests__/LoginForm.test.tsx
+++ src/__tests__/LoginForm.test.tsx
@@ -1,7 +1,12 @@
 import React from "react";
-import {cleanup, render, RenderResult} from "@testing-library/react";
+import {
+  cleanup,
+  fireEvent,
+  render,
+  RenderResult} from "@testing-library/react";
 import {act} from "react-dom/test-utils";
 import {LoginForm} from "../components/LoginForm";
+import {GlobalProvider} from "../contexts/GlobalStateContext";
 
 describe('LoginPage', () => {
 
@@ -9,8 +14,10 @@ describe('LoginPage', () => {
 
   beforeEach(() => {
     act(() => {
-      render(
-        <LoginForm history={{}}/>
+      wrapper = render(
+        <GlobalProvider
+          children={<LoginForm history={{}}/>}
+        />
       );
     });
   });
@@ -20,7 +27,29 @@ describe('LoginPage', () => {
   });
 
 
-  test("When ", function () {
+  test(`When the login form is submitted
+  Then a loading spinner should appear`, function () {
+    act(() => {
+      fireEvent.change(wrapper.getByLabelText(/email/i), {
+        target: {
+          value: "tester@gmail.com",
+        }
+      });
+    });
+
+    act(() => {
+      fireEvent.change(wrapper.getByLabelText(/password/i), {
+        target: {
+          value: "password",
+        }
+      });
+    });
+
+    act(() => {
+      fireEvent.click(wrapper.getByText(/submit/i));
+    });
+
+    wrapper.getByTestId("loading-spinner");
 
   });
 });
