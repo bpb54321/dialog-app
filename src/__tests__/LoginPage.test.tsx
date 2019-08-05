328ae78bfec1214919a300ffb67cbc9a07a2c67d
Brian Blosser <bpb54321@gmail.com>
2019-08-04 20:19:57 -0400

    Added a test for the LoginPage.

---
+++ src/__tests__/LoginPage.test.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {cleanup, render, RenderResult} from "@testing-library/react";
+import {act} from "react-dom/test-utils";
+
+describe('LoginPage', () => {
+
+  let wrapper: RenderResult;
+
+  beforeEach(() => {
+    act(() => {
+
+    });
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+
+
+  test("When ", function () {
+
+  });
+});
+
