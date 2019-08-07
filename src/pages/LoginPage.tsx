0cd63ec14887f1f1e9d0caf6eb01b667ef099fa3
Brian Blosser <bpb54321@gmail.com>
2019-08-07 09:15:28 -0400

    Removed withLoadingSpinner from LoginPage.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,20 +1,16 @@
 import React, {FunctionComponent} from 'react';
-import {LoginForm, LoginFormProps} from "../components/LoginForm";
+import {LoginForm} from "../components/LoginForm";
 import {Link} from "react-router-dom";
-import {withLoadingSpinner} from "../higher-order-components/withLoadingSpinner";
-
 
 interface Props {
   history: any;
 }
 
-const LoginFormWithLoadingSpinner = withLoadingSpinner<LoginFormProps>(LoginForm);
-
 export const LoginPage: FunctionComponent<Props> = (props) => {
   return (
     <>
       <h1>Login</h1>
-      <LoginFormWithLoadingSpinner history={props.history} />
+      <LoginForm history={props.history} />
       <Link to={"/sign-up"}>Sign Up</Link>
     </>
   );
