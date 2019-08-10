6a732f1b771ef39d563431e4ab828f9dc12e31bb
Brian Blosser <bpb54321@gmail.com>
2019-08-10 18:20:23 -0400

    Added a first test, after figuring out how to seed the database.

--- cypress/integration/display-lines.spec.js
+++ cypress/integration/display-lines.spec.js
@@ -1,6 +1,22 @@
 describe("Smoke test", () => {
+
+    // user token
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjano1aWlkbGIwMDAzMDc2NmNxYzJwbDFvIiwiaWF0IjoxNTY1NDM5OTIxfQ.DGZ3m6mZftHS5LODRKhl80DzHfFPXyxzpE-vKgHdQKY";
+
     it("Makes sure the welcome message comes up", () => {
-        cy.visit("http://localhost:3000");
-        cy.contains("Hello, world!");
+
+        cy.exec(`cat ${Cypress.env('sql_dump_directory')}single-user.sql | ` +
+            `docker exec -i ${Cypress.env('docker_mysql_service_name')} ` +
+            `mysql -uroot -p${Cypress.env('docker_mysql_password')} ${Cypress.env('docker_mysql_db_name')}`);
+
+        cy.clearLocalStorage();
+        cy.visit('/', {
+            onBeforeLoad: function(window){
+                // and before the page finishes loading
+                // set the id_token in local storage
+                window.sessionStorage.setItem('token', token);
+            }
+        });
+        cy.contains("Dialogs");
     })
 });
