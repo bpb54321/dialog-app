8b35d211e14860164d986ebab1787044b1f39743
Brian Blosser <bpb54321@gmail.com>
2019-08-11 09:49:18 -0400

    Added @testing-library/cypress, refactored the Dialog Edit e2e test.

--- cypress/e2e/display-lines.spec.js
+++ cypress/e2e/display-lines.spec.js
@@ -1,15 +1,25 @@
-describe("Smoke test", () => {
+describe("Dialog Edit Page", () => {
 
     // user token
     const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjano1aWlkbGIwMDAzMDc2NmNxYzJwbDFvIiwiaWF0IjoxNTY1NDM5OTIxfQ.DGZ3m6mZftHS5LODRKhl80DzHfFPXyxzpE-vKgHdQKY";
 
-    it("Makes sure the welcome message comes up", () => {
-
+    beforeEach(() => {
+        // Load database with one user whose token corresponds to the above token
         cy.exec(`cat ${Cypress.env('sql_dump_directory')}single-user.sql | ` +
-            `docker exec -i ${Cypress.env('docker_mysql_service_name')} ` +
-            `mysql -uroot -p${Cypress.env('docker_mysql_password')} ${Cypress.env('docker_mysql_db_name')}`);
+          `docker exec -i ${Cypress.env('docker_mysql_service_name')} ` +
+          `mysql -uroot -p${Cypress.env('docker_mysql_password')} ${Cypress.env('docker_mysql_db_name')}`);
 
+        // Clear local storage of the user's token
         cy.clearLocalStorage();
+    });
+
+    specify(`Given a dialog which has 0 lines
+        And the dialog has two roles
+        When we add lines to the dialog
+        Then the lines should appear on the page
+        And their line numbers should correspond to the order in which they were added.`, () => {
+
+        // Login directly using token
         cy.visit('/', {
             onBeforeLoad: function(window){
                 // and before the page finishes loading
@@ -17,6 +27,8 @@ describe("Smoke test", () => {
                 window.sessionStorage.setItem('token', token);
             }
         });
+
+
         cy.contains("Dialogs");
     })
 });
