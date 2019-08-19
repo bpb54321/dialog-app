a847415ed5bcdd47856887b37eb94702ded676e2
Brian Blosser <bpb54321@gmail.com>
2019-08-18 21:43:26 -0400

    Put all front-end code in the front-end folder

---
+++ front-end/build/precache-manifest.aa2dc91008bc6352cecd534261ee2c79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc07fb78a0cde0c8deea3265afe48305",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7da233e93db5112ebea6",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "d3731a18056a9bd4a71e",
+    "url": "/static/css/main.a82e925b.chunk.css"
+  },
+  {
+    "revision": "7da233e93db5112ebea6",
+    "url": "/static/js/2.ad8c7587.chunk.js"
+  },
+  {
+    "revision": "d3731a18056a9bd4a71e",
+    "url": "/static/js/main.eeadf40b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
